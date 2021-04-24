ca72d13917fbc501c38c4cc360298d873efe67d1
siddharthkp <siddharth.kshetrapal@gmail.com>
2021-04-24 11:59:11 +0200

    ui-devtools

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button onClick={() => setCount(count - 1)}> - </button>
+          {count}
+          <button onClick={() => setCount(count + 1)}> + </button>
+        </div>
       </header>
+      <main></main>
     </div>
   );
 }
